60f4e24b21edda0c11c44f6a16d92bde3cd9d7b3
juan052115 <juan052115@gmail.com>
2024-05-20 21:56:42 +0000

    Ejecutar el repositorio en IDX con una conexion https

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    https: true,
+    port: 8081,
+    client: {
+      webSocketURL: 'wss://10.88.0.4:8081/ws',
+    },
+  },
 })
